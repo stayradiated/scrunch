478970ceee87068259bfde1cec46480d3544241c
George Czabania <george@czabania.com>
2013-10-06 18:28:39 +1300

    Update to version 0.1.1 and refactor bin

--- bin/scrunch.js
+++ bin/scrunch.js
@@ -4,13 +4,14 @@
 
     "use strict";
 
-    var Scrunch, lodash, utils, fs,
+    var Scrunch, lodash, utils, fs, npmPackage,
         options, fileOut, compile, folderName;
 
-    Scrunch = require('../lib/scrunch');
-    lodash = require('lodash');
-    utils = require('../lib/utils');
-    fs = require('fs');
+    Scrunch    = require('../lib/scrunch');
+    lodash     = require('lodash');
+    utils      = require('../lib/utils');
+    fs         = require('fs');
+    npmPackage = require('../package.json');
 
     /**
      * Init function
@@ -19,19 +20,19 @@
     compile = function (options) {
         var scrunch = new Scrunch(options);
 
-        scrunch.vent.on('run', function () {
-            scrunch.compile(options);
+        scrunch.vent.on('init', function () {
+            scrunch.scrunch(options);
         });
 
-        scrunch.vent.on('compile', function (output) {
-            console.log('...compiled');
+        scrunch.vent.on('scrunch', function (output) {
+            console.log('...scrunched');
             if (options.fileOut) {
-                console.log('writing');
+                console.log('Saving to ' + options.fileOut);
                 fs.writeFile(options.fileOut, output, options.fn);
             }
         });
 
-        scrunch.run();
+        scrunch.init();
 
         return scrunch.output;
 
@@ -40,7 +41,15 @@
 
     if (process.argv.length < 3) {
 
-        console.log('Usage: scrunch file [--log] [--compile] [[--watch] --out file]');
+        console.log('Usage: scrunch FILE [OPTION]\n' +
+'\n' +
+'Options:\n' +
+'   -v --vebose\n' +
+'   -c --compile\n' +
+'   -w --watch\n' +
+'   -o [file]\n' +
+'\n' +
+'Version: ' + npmPackage.version);
 
     } else {
 
@@ -48,19 +57,19 @@
             path: process.argv[2]
         };
 
-        if (process.argv.indexOf('--log') > -1) {
+        if (utils.containsAny(process.argv, '-l', '--log')) {
             options.verbose = true;
         }
 
-        if (process.argv.indexOf('--compile') > -1) {
+        if (utils.containsAny(process.argv, '-c', '--compile')) {
             options.compile = true;
         }
 
-        if (process.argv.indexOf('--watch') > -1) {
+        if (utils.containsAny(process.argv, '-w', '--watch')) {
             options.watch = true;
         }
 
-        fileOut = process.argv.indexOf('--out');
+        fileOut = process.argv.indexOf('-o');
         if (fileOut > -1) {
             options.fileOut = process.argv[fileOut + 1];
             compile(options);

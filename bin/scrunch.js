19ab29230926145df5da126962ba4dacfdcf2bea
George Czabania <george@czabania.com>
2014-02-04 00:06:01 +1300

    Completely simplify bin/scrunch.js

--- bin/scrunch.js
+++ bin/scrunch.js
@@ -1,84 +1,27 @@
 #!/usr/bin/env node
 
-(function () {
+'use strict';
 
-    "use strict";
+var scrunch, fs, npmPackage, program;
 
-    var Scrunch, lodash, utils, fs, npmPackage,
-        options, fileOut, compile, folderName;
+scrunch    = require('../index');
+fs         = require('fs');
+npmPackage = require('../package.json');
+program    = require('commander');
 
-    Scrunch    = require('../lib/scrunch');
-    lodash     = require('lodash');
-    utils      = require('../lib/utils');
-    fs         = require('fs');
-    npmPackage = require('../package.json');
+program
+  .version(npmPackage.version)
+  .option('-v, --verbose', 'Detailed logs')
+  .option('-i, --in [file]', 'Input file')
+  .option('-o, --out [file]', 'Write to file')
+  .parse(process.argv);
 
-    /**
-     * Init function
-     */
+if (! program.in || ! program.out) {
+  return console.log('Must specify input and output files');
+}
 
-    compile = function (options) {
-        var scrunch = new Scrunch(options);
-
-        scrunch.vent.on('init', function () {
-            scrunch.scrunch(options);
-        });
-
-        scrunch.vent.on('scrunch', function (output) {
-            console.log('...scrunched');
-            if (options.fileOut) {
-                console.log('Saving to ' + options.fileOut);
-                fs.writeFile(options.fileOut, output, options.fn);
-            }
-        });
-
-        scrunch.init();
-
-        return scrunch.output;
-
-    };
-
-
-    if (process.argv.length < 3) {
-
-        console.log('Usage: scrunch FILE [OPTION]\n' +
-'\n' +
-'Options:\n' +
-'   -v --vebose\n' +
-'   -c --compile\n' +
-'   -w --watch\n' +
-'   -o [file]\n' +
-'\n' +
-'Version: ' + npmPackage.version);
-
-    } else {
-
-        options = {
-            path: process.argv[2]
-        };
-
-        if (utils.containsAny(process.argv, '-l', '--log')) {
-            options.verbose = true;
-        }
-
-        if (utils.containsAny(process.argv, '-c', '--compile')) {
-            options.compile = true;
-        }
-
-        if (utils.containsAny(process.argv, '-w', '--watch')) {
-            options.watch = true;
-        }
-
-        fileOut = process.argv.indexOf('-o');
-        if (fileOut > -1) {
-            options.fileOut = process.argv[fileOut + 1];
-            compile(options);
-        } else if (options.watch) {
-            console.log('[error] You must specify an output file to use --watch');
-        } else {
-            console.log('\n' + compile(options));
-        }
-
-    }
-
-}());
+scrunch({
+  input: program.in
+}).then(function (data) {
+  fs.writeFile(program.out, data);
+});
